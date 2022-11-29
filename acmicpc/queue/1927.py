8ef891df59bbab1de34604d525905de528aad577
0strich <gee8195@naver.com>
2022-11-30 00:14:47 +0900

    queue update

---
+++ acmicpc/queue/1927.py
@@ -0,0 +1,11 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+heap = []
+output = []
+for i in range(N):
+	x = int(sys.stdin.readline())
+	if x: heapq.heappush(heap, x)
+	else: output.append(heapq.heappop(heap)) if heap else output.append(0)
+print("\n".join(map(str, output)))
